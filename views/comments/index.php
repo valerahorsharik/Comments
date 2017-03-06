347d943e1ad7765df9473db3c23bdc9315639726
valerahorsharik <valera.horsharik@gmail.com>
2017-03-06 13:25:41 +0200

    Added a simple template.

--- views/comments/index.php
+++ views/comments/index.php
@@ -1,4 +1,10 @@
-<form>
+<form method="post" action="/comment/save">
     <textarea name="comment" placeholder="Input your comment here..."></textarea>
     <input type="submit" value="Send">
 </form>
+<?php foreach ($data['comments'] as $comment):?>
+    <div class="message" data-id="<?=$comment['id']?>">
+        <div class="date"><?=$comment['timestamp']?></div>
+        <div class="text"><?=$comment['text']?></div>
+    </div>
+<?php endforeach; ?>
