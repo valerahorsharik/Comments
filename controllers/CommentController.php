c7d1cdd574d754f0dc8a42207a42c27fce63f4d9
valerahorsharik <valera.horsharik@gmail.com>
2017-03-06 13:25:22 +0200

    Added a save method.

--- controllers/CommentController.php
+++ controllers/CommentController.php
@@ -7,8 +7,24 @@ use models\Comments as Comments;
 
 class CommentController extends Controller{
     
+    /**
+     * 
+     * Display main comments
+     * 
+     */
     public function index() {
         $comments = Comments::getMainComments();
         $this->view->display('comments/index', ['comments' => $comments]);
+    }
+    
+    /**
+     * 
+     * Save a new comment
+     * 
+     */
+    public function save(){
+        $comment = getPost('comment');
+        $authorId = $_SESSION['user']['id'];
+        Comments::save($authorId, $comment);
     }
 }
