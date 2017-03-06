90c09bad9b1e68f7dd97c44e774cf824ba2d1ab2
valerahorsharik <valera.horsharik@gmail.com>
2017-03-07 00:52:50 +0200

    Slightly changed the view.

--- views/comments/index.php
+++ views/comments/index.php
@@ -1,10 +1,26 @@
-<form method="post" action="/comment/save">
-    <textarea name="comment" placeholder="Input your comment here..."></textarea>
+<form class='comments-form' method="post" action="/comment/save">
+    <textarea rows='4' name="comment" placeholder="Input your comment here..."></textarea>
     <input type="submit" value="Send">
 </form>
-<?php foreach ($data['comments'] as $comment):?>
-    <div class="message" data-id="<?=$comment['id']?>">
-        <div class="date"><?=$comment['timestamp']?></div>
-        <div class="text"><?=$comment['text']?></div>
-    </div>
-<?php endforeach; ?>
+<div class="comments">
+    <ul class="comments-list" data-lvl='0'>
+        <?php foreach ($data['comments'] as $comment): ?>
+            <li data-comment-id='<?= $comment['id'] ?>'>
+                <div class="comment-info">
+                    <div class="info"><?= $comment['timestamp'] ?></div>
+                    <div class="actions">
+                        <?php if (!$data['guest']): ?>
+                            <?php if ($comment['user_id'] == $_SESSION['user']['id']): ?>
+                                <span class='glyphicon glyphicon-edit edit-comment'></span>
+                                <span class='glyphicon glyphicon-remove delete-comment'></span>
+                            <?php endif; ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <div class="comment-text">
+                    <?= $comment['text'] ?>
+                </div>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
