5cc6a99741dbf469dc6dd862f611d28aba725e5d
valerahorsharik <valera.horsharik@gmail.com>
2017-03-07 00:55:08 +0200

    Added a save method.

--- controllers/CommentController.php
+++ controllers/CommentController.php
@@ -4,6 +4,7 @@ namespace controllers;
 
 use core\Controller as Controller;
 use models\Comments as Comments;
+use controllers\UserController as User;
 
 class CommentController extends Controller{
     
@@ -13,8 +14,10 @@ class CommentController extends Controller{
      * 
      */
     public function index() {
+        $guest = User::isGuest();
         $comments = Comments::getMainComments();
-        $this->view->display('comments/index', ['comments' => $comments]);
+        $this->view->display('comments/index', ['comments' => $comments,
+                                                'guest' => $guest]);
     }
     
     /**
@@ -23,8 +26,29 @@ class CommentController extends Controller{
      * 
      */
     public function save(){
+        $this->notAuthorized();
         $comment = getPost('comment');
+        $parentId = getPost('parentId');
         $authorId = $_SESSION['user']['id'];
-        Comments::save($authorId, $comment);
+        $date = date("Y-m-d H:i:s");
+        $commentId = Comments::save($authorId, $comment, $parentId);
+        header('HTTP/1.1 200 OK');
+        header('Content-Type: application/json; charset=UTF-8');
+        echo json_encode(['date' => $date,'commentId' => $commentId]);
+
+    }
+    
+    /**
+     * 
+     * Checking, if user not authorized
+     * send an error with 403 status
+     * 
+     * @return string
+     */
+    private function notAuthorized(){
+        if (User::isGuest()){
+            header('HTTP/1.1 403 Forbidden');
+            die("You need <a href=\"/login\">login</a> first.");
+        }
     }
 }
