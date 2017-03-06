cef6195b6e559b8b8e0fe59d3e910eb6052a5cb0
valerahorsharik <valera.horsharik@gmail.com>
2017-03-07 01:49:44 +0200

    Added a delete method.

--- controllers/CommentController.php
+++ controllers/CommentController.php
@@ -12,6 +12,7 @@ class CommentController extends Controller{
      * 
      * Display main comments
      * 
+     * @return void
      */
     public function index() {
         $guest = User::isGuest();
@@ -24,6 +25,7 @@ class CommentController extends Controller{
      * 
      * Save a new comment
      * 
+     * @return void
      */
     public function save(){
         $this->notAuthorized();
@@ -40,6 +42,20 @@ class CommentController extends Controller{
     
     /**
      * 
+     * Delete a comment
+     * 
+     * @return void
+     */
+    public function delete(){
+        $this->notAuthorized();
+        $commentId = getPost('commentId');
+        $this->isOwner($commentId);
+        Comments::deleteComment($commentId);
+        header('HTTP/1.1 200 OK');
+    }
+    
+    /**
+     * 
      * Checking, if user not authorized
      * send an error with 403 status
      * 
@@ -50,5 +66,22 @@ class CommentController extends Controller{
             header('HTTP/1.1 403 Forbidden');
             die("You need <a href=\"/login\">login</a> first.");
         }
+    }
+    
+    /**
+     * 
+     * Checking if the user is author of the comment
+     * 
+     * @param int $commentId ID of the comment,which we wanna delete
+     * 
+     * @return boolean
+     */
+    private function isOwner($commentId){
+        $userId = Comments::getUserId($commentId);
+        if ($userId != $_SESSION['user']['id']){
+            header('HTTP/1.1 403 Forbidden');
+            die("Sorry, but you are not owner of the comment");
+        }
+        return true;
     }
 }
