6b9d36773203943bd7b9ea891ecdd121fa7a3b1b
valerahorsharik <valera.horsharik@gmail.com>
2017-03-07 05:13:46 +0200

    Added a button for commenting existing comments.

--- views/comments/index.php
+++ views/comments/index.php
@@ -1,4 +1,4 @@
-<form class='comments-form' method="post" action="/comment/save">
+<form class='comments-form new' method="post" action="/comment/save">
     <textarea rows='4' name="comment" placeholder="Input your comment here..."></textarea>
     <input type="submit" value="Send">
 </form>
@@ -10,6 +10,7 @@
                     <div class="info"><?= $comment['timestamp'] ?></div>
                     <div class="actions">
                         <?php if (!$data['guest']): ?>
+                            <span class='glyphicon glyphicon-comment comment-comment'></span>
                             <?php if ($comment['user_id'] == $_SESSION['user']['id']): ?>
                                 <span class='glyphicon glyphicon-edit edit-comment'></span>
                                 <span class='glyphicon glyphicon-remove delete-comment'></span>
