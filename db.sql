f47d50fc8867d92e2de7411c8dc313d4d35f1b85
valerahorsharik <valera.horsharik@gmail.com>
2017-03-07 06:21:13 +0200

    New version.

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 10.0.2.9
--- Время создания: Мар 03 2017 г., 17:29
+-- Время создания: Мар 07 2017 г., 07:20
 -- Версия сервера: 5.6.32-78.0-log
 -- Версия PHP: 5.4.45
 
@@ -23,44 +23,84 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `comments`
+--
+
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `parent_id` int(11) NOT NULL DEFAULT '0',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `text` text NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM AUTO_INCREMENT=43 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `comments`
+--
+
+INSERT INTO `comments` (`id`, `user_id`, `parent_id`, `timestamp`, `text`, `deleted`) VALUES
+(1, 25, 0, '2017-03-06 10:41:22', 'hello, it is the first comment.!', 0),
+(2, 25, 0, '2017-03-07 02:19:08', 'pew pewpew123             sdasda                       pew pewpew123                      sdas1231231231232131da                       pew pewpew123', 0),
+(35, 25, 2, '2017-03-07 04:19:12', 'second', 0),
+(36, 25, 2, '2017-03-07 04:19:20', 'second', 0),
+(40, 25, 35, '2017-03-07 04:02:01', 'third', 0),
+(42, 25, 40, '2017-03-07 04:18:42', 'fourth', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL,
-  `first_name` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `last_name` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `photo` text CHARACTER SET utf8 NOT NULL,
-  `vk_id` int(11) DEFAULT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=cp1251;
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `photo` text NOT NULL,
+  `vk_id` int(11) DEFAULT NULL,
+  `fb_id` int(11) DEFAULT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `photo`, `vk_id`) VALUES
-(1, 'Р’Р°Р»РµСЂР°', 'Р“РѕСЂС€Р°СЂРёРє', 'valera.horsharik@gmail.com', 'https://pp.userapi.com/c627428/v627428161/34d82/KXyok1X-Cnk.jpg', 11972161);
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `photo`, `vk_id`, `fb_id`) VALUES
+(25, 'Валера', 'Горшарик', 'valera.horsharik@gmail.com', 'https://pp.userapi.com/c627428/v627428161/34d82/KXyok1X-Cnk.jpg', 11972161, 2147483647);
 
 --
 -- Индексы сохранённых таблиц
 --
 
 --
+-- Индексы таблицы `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
 --
+-- AUTO_INCREMENT для таблицы `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=43;
+--
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=26;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
