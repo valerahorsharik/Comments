f76a579d7308ea1723f40007b6507538d26a6200
valerahorsharik <valera.horsharik@gmail.com>
2017-03-07 03:36:04 +0200

    Added a route for editing comments.

--- config/routes.php
+++ config/routes.php
@@ -12,5 +12,6 @@ return array(
     //Comment's routes
     'comment/save' => 'comment/save',
     'comment/delete' => 'comment/delete',
+    'comment/edit' => 'comment/edit',
     '' => 'comment/index',
 );
